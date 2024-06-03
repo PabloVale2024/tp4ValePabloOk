6954f9ecd6254407561292c4aeb042b811a3ea47
PabloVale2024 <166053449+PabloVale2024@users.noreply.github.com>
2024-06-03 20:01:40 -0300

    listado de carreras

--- src/main/java/ar/edu/unju/fi/controller/CarreraController.java
+++ src/main/java/ar/edu/unju/fi/controller/CarreraController.java
@@ -2,8 +2,11 @@ package ar.edu.unju.fi.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import ar.edu.unju.fi.collections.ListadoCarreras;
 import ar.edu.unju.fi.model.Carrera;
 @Controller
 public class CarreraController {
@@ -13,6 +16,15 @@ public class CarreraController {
 		ModelAndView modelView = new ModelAndView("formCarrera");
 		//agrega objeto
 		modelView.addObject("nuevaCarrera",new Carrera());
+		return modelView;
+	}
+	@PostMapping("/guardarCarrera")
+	public ModelAndView saveCarrera(@ModelAttribute("nuevaCarrera") Carrera carreraAGuardar) {
+		//guardar
+		ListadoCarreras.agregarCarrera(carreraAGuardar);
+		//mostrar el listado
+		ModelAndView modelView = new ModelAndView("listadoDeCarreras");
+		modelView.addObject("listadoCarreras",ListadoCarreras.listarCarreras());
 		return modelView;
 	}
 }
